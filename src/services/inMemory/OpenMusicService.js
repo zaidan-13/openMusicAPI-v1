f69828ec65dc1a685d1b47b78d0008cbbef5b05f
zaidan-13 <zerreff.io@gmail.com>
2023-12-23 14:23:40 +0700

    add InMemory Services

---
+++ src/services/inMemory/OpenMusicService.js
@@ -0,0 +1,65 @@
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class NotesService {
+    constructor() {
+        this._music = [];
+    }
+
+    addNote({ title,  year, genre, performer, duration}) {
+        const id = nanoid(16);
+
+        const newMusic = {
+            title,  year, genre, performer, duration
+        };
+
+        this._music.push(newMusic);
+
+        const isSuccess = this._music.filter((song) => song.id === id).length > 0;
+
+        if (!isSuccess) {
+            throw new InvariantError('Catatan gagal ditambahkan');
+        }
+
+        return id;
+    }
+
+    getNotes() {
+        return this._music;
+    }
+
+    getNoteById(id) {
+        const song = this._music.filter((n) => n.id === id)[0];
+
+        if (!song) {
+            throw new NotFoundError('Catatan tidak ditemukan');
+        }
+        return song;
+    }
+
+    editNoteById(id, { title,  year, genre, performer, duration }) {
+        const index = this._music.findIndex((song) => song.id === id);
+
+        if (index === -1) {
+            throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
+        }
+
+        this._music[index] = {
+            ...this._music[index],
+            title,  year, genre, performer, duration
+        };
+    }
+
+    deleteNoteById(id) {
+        const index = this._music.findIndex((song) => song.id === id);
+
+        if (index === -1) {
+            throw new NotFoundError('Catatan gagal dihapus. id tidak ditemukan');
+        }
+
+        this._music.splice(index, 1);
+    }
+}
+
+module.exports = NotesService;
